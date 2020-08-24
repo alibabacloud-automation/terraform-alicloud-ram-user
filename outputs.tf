238fa8629ebc5168f4bb1a03eca817d2ca06e8ef
Brian BORDINI <52781411+brianmori@users.noreply.github.com>
2020-08-24 14:52:39 +0200

    added outputs with this

--- outputs.tf
+++ outputs.tf
@@ -1,4 +1,31 @@
 
+output "this_users_id" {
+  description = "The users created"
+  value       = [for value in alicloud_ram_user.this : value]
+}
+
+output "this_access_keys" {
+  description = "The access keys created"
+  value       = [for value in alicloud_ram_access_key.this : value]
+}
+
+output "this_secret_keys" {
+  description = "The secret keys created"
+  value       = [for value in fileset(path.cwd, "*.key") : file(value)]
+}
+
+output "this_user_system_policy_attachments" {
+  description = "The System policies attached to the users"
+  value       = [for value in alicloud_ram_user_policy_attachment.system_policies : value]
+}
+
+output "this_user_custom_policy_attachments" {
+  description = "The Custom policies attached to the users"
+  value       = [for value in alicloud_ram_user_policy_attachment.custom_policies : value]
+}
+
+###
+
 output "users_id" {
   description = "The users created"
   value       = [for value in alicloud_ram_user.this : value]
