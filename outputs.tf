534e225d3bda4c4becd0b97820b8535808a71aa2
Brian BORDINI <52781411+brianmori@users.noreply.github.com>
2020-08-28 17:54:57 +0200

    deleted duplicated outputs

--- outputs.tf
+++ outputs.tf
@@ -1,31 +1,3 @@
-
-output "this_users_id" {
-  description = "The users created"
-  value       = [for value in alicloud_ram_user.this : value]
-}
-
-output "this_access_keys" {
-  description = "The access keys created"
-  value       = [for value in alicloud_ram_access_key.this : value]
-}
-
-output "this_secret_keys" {
-  description = "The secret keys created"
-  value       = [for value in fileset(path.cwd, "*.key") : file(value)]
-}
-
-output "this_user_system_policy_attachments" {
-  description = "The System policies attached to the users"
-  value       = [for value in alicloud_ram_user_policy_attachment.system : value]
-}
-
-output "this_user_custom_policy_attachments" {
-  description = "The Custom policies attached to the users"
-  value       = [for value in alicloud_ram_user_policy_attachment.custom : value]
-}
-
-###
-
 output "users_id" {
   description = "The users created"
   value       = [for value in alicloud_ram_user.this : value]
